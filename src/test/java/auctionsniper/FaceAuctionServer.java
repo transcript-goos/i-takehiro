1adc54f89b1827ab0e21bfb7c6aa1e5dbe81b79f
i-takehiro <takehiro.agile@gmail.com>
2012-11-06 21:57:36 +0900

    FaceAuctionServer の実装
    第11章 P96-97

--- src/test/java/auctionsniper/FaceAuctionServer.java
+++ src/test/java/auctionsniper/FaceAuctionServer.java
@@ -1,14 +1,44 @@
 package auctionsniper;
 
+import java.text.Format;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
 public class FaceAuctionServer {
 
-    public FaceAuctionServer(String string) {
-        // TODO Auto-generated constructor stub
+    public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    public static final String AUCTION_RESOURCE = "Auction";
+    public static final String XMPP_HOSTNAME = "localhost";
+    private static final String AUCTION_PASSWORD = "auction";
+
+    private final String itemId;
+    private final XMPPConnection connection;
+    private Chat currentChat;
+
+    public FaceAuctionServer(final String itemId) {
+        this.itemId = itemId;
+        this.connection = new XMPPConnection(XMPP_HOSTNAME);
     }
 
-    public void startSellingItem() {
-        // TODO Auto-generated method stub
+    public void startSellingItem() throws XMPPException {
+        this.connection.connect();
+        this.connection.login(String.format(ITEM_ID_AS_LOGIN, this.itemId),
+                AUCTION_PASSWORD, AUCTION_RESOURCE);
+        this.connection.getChatManager().addChatListener(
+                new ChatManagerListener() {
+                    @Override
+                    public void chatCreated(final Chat chat,
+                            final boolean createdLocally) {
+                        FaceAuctionServer.this.currentChat = chat;
+                    }
+                });
+    }
 
+    public String getItemId() {
+        return this.itemId;
     }
 
     public void hasReceivedJoinRequestFromSniper() {
@@ -24,11 +54,6 @@ public class FaceAuctionServer {
     public void stop() {
         // TODO Auto-generated method stub
 
-    }
-
-    public String getItemId() {
-        // TODO Auto-generated method stub
-        return null;
     }
 
 }
