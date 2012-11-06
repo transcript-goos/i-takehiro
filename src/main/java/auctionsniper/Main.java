a3a8efb52b2a5bba1d6716cf9f95c1dfabff0ebc
i-takehiro <takehiro.agile@gmail.com>
2012-11-06 21:35:28 +0900

    AuctionSniperDriver の実装
    第11章 P95-96

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -2,6 +2,8 @@ package auctionsniper;
 
 public class Main {
 
+    public static final String MAIN_WINDOW_NAME = null;
+
     public static void main(String... args) {
         // TODO Auto-generated method stub
 
--- src/test/java/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/auctionsniper/AuctionSniperDriver.java
@@ -1,19 +1,24 @@
 package auctionsniper;
 
-public class AuctionSniperDriver {
+import static org.hamcrest.CoreMatchers.equalTo;
 
-    public AuctionSniperDriver(int i) {
-        // TODO Auto-generated constructor stub
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+public class AuctionSniperDriver extends JFrameDriver {
+
+    @SuppressWarnings("unchecked")
+    public AuctionSniperDriver(final int timeoutMillis) {
+        super(new GesturePerformer(), JFrameDriver.topLevelFrame(
+                named(Main.MAIN_WINDOW_NAME), showingOnScreen()),
+                new AWTEventQueueProber(timeoutMillis, 100));
     }
 
-    public void dispose() {
-        // TODO Auto-generated method stub
-        
+    @SuppressWarnings("unchecked")
+    public void showsSniperStatus(final String statusText) {
+        new JLabelDriver(this, named(Main.MAIN_WINDOW_NAME))
+                .hasText(equalTo(statusText));
     }
-
-    public void showsSniperStatus(String statusJoining) {
-        // TODO Auto-generated method stub
-        
-    }
-
 }
