76947fafdc89839129fd8ed51826cad4a6c4e52b
i-takehiro <takehiro.agile@gmail.com>
2012-11-06 22:54:24 +0900

    Main の実装追加
    第11章 P101

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -1,12 +1,28 @@
 package auctionsniper;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
 
     public static final String MAIN_WINDOW_NAME = null;
 
-    public static void main(String... args) {
-        // TODO Auto-generated method stub
+    private MainWindow ui;
 
+    public Main() throws Exception {
+        startUserInterface();
     }
 
+    public static void main(final String... args) throws Exception {
+        final Main main = new Main();
+    }
+
+    private void startUserInterface() throws Exception {
+        SwingUtilities.invokeAndWait(new Runnable() {
+
+            @Override
+            public void run() {
+                ui = new MainWindow();
+            }
+        });
+    }
 }
