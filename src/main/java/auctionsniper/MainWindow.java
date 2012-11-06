8744f696d99e1c0ba4e3e2019d0b7fd237b645b9
i-takehiro <takehiro.agile@gmail.com>
2012-11-06 23:39:29 +0900

    スナイパーの状態を表示するラベルをMainWindowに追加
    第11章 P102

--- src/main/java/auctionsniper/MainWindow.java
+++ src/main/java/auctionsniper/MainWindow.java
@@ -1,6 +1,10 @@
 package auctionsniper;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
 
 public class MainWindow extends JFrame {
 
@@ -8,10 +12,21 @@ public class MainWindow extends JFrame {
 
     public static final String SNIPER_STATUS_NAME = "sniper status";
 
+    private final JLabel sniperStatus = createLabel(ApplicationRunner.STATUS_JOINING);
+
     public MainWindow() {
         super("Auction Sniper");
         setName(Main.MAIN_WINDOW_NAME);
+        add(sniperStatus);
+        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private static JLabel createLabel(final String initialText) {
+        final JLabel result = new JLabel(initialText);
+        result.setName(SNIPER_STATUS_NAME);
+        result.setBorder(new LineBorder(Color.BLACK));
+        return result;
     }
 }
